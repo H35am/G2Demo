b0fb7edd24cc5ee19d3545eb41feb28e66c9fbce
heszar@gmail.com <heszar@gmail.com>
2014-12-18 18:28:15 +0100

    game 2

---
+++ TreinVanafBoven.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TreinVanafBoven here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TreinVanafBoven extends Trein
+{
+    /**
+     * Act - do whatever the TreinVanafBoven wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ Vrachtauto.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Vrachtauto here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Vrachtauto extends Voertuigen
+{
+    /**
+     * Act - do whatever the Vrachtauto wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
